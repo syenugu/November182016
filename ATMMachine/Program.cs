1593afce803bd21c8ece08bc8c7ca281b42d6ff7
Suresh Yenugu <suresh.yenugu@campsystems.com>
2016-12-05 16:16:07 +0530

    First commit

---
+++ ATMMachine/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace ATMMachine
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("--------------CAMP ATM----------------");
+            ShowOperationTypeScreen();
+            var oppType = (OperationType)ReadInput();
+            ShowAccountTypeScreen();
+            var accType = (AccountType)ReadInput();
+            PerformOperation(accType, oppType);
+            Console.ReadLine();
+        }
+
+        private static void ShowOperationTypeScreen()
+        {
+            Console.WriteLine("-----Choose your Operation Type-------");
+            Console.WriteLine("                          Deposit -> 1");
+            Console.WriteLine("                         Withdraw -> 2");
+            Console.WriteLine("                  Balance Enquiry -> 3");
+        }
+
+        private static void ShowAccountTypeScreen()
+        {
+            Console.WriteLine("------Choose your Account Type--------");
+            Console.WriteLine("                           Active -> 1");
+            Console.WriteLine("                           Closed -> 2");
+            Console.WriteLine("                     In-Operative -> 3");
+        }
+
+        private static int ReadInput()
+        {
+            int input;
+            bool inValid;
+            do
+            {
+                inValid = false;
+                Console.Write("                                     ");
+                if (!int.TryParse(Console.ReadLine(), out input))
+                {
+                    Console.WriteLine("Input should be a numaric value");
+                    inValid = true;
+                }
+                else if (input < 1 || input > 3)
+                {
+                    Console.WriteLine("Input should be among 1, 2, 3");
+                    inValid = true;
+                }
+            } while (inValid);
+            return input;
+        }
+
+        private static void PerformOperation(AccountType accType, OperationType oppType)
+        {
+            switch (accType)
+            {
+                case AccountType.Active:
+                    Console.WriteLine("                                  True");
+                    Console.WriteLine("                                Active");
+                    break;
+                case AccountType.Operative:
+                    if (oppType.Equals(OperationType.BalanceEnquiry))
+                    {
+                        Console.WriteLine("                                  True");
+                        Console.WriteLine("                           InOperative");
+                    }
+                    else
+                    {
+                        Console.WriteLine("                                  True");
+                        Console.WriteLine("                                Active");
+                    }
+                    break;
+                case AccountType.Closed:
+                    Console.WriteLine("                                 False");
+                    Console.WriteLine("                                Closed");
+                    break;
+            }
+        }
+    }
+
+    public enum AccountType
+    {
+        Active = 1,
+        Closed = 2,
+        Operative = 3
+    }
+
+    public enum OperationType
+    {
+        Deposit = 1,
+        Withdraw = 2,
+        BalanceEnquiry = 3
+    }
+}
---
+++ ATMMachine/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ATMMachine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAMP Systems Pvt Ltd")]
+[assembly: AssemblyProduct("ATMMachine")]
+[assembly: AssemblyCopyright("Copyright © CAMP Systems Pvt Ltd 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("782652dc-0186-4821-b65f-3fb01a9cca85")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
